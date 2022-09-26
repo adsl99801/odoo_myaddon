b4809e5d541ca91ef6be96e7b08b419334343890
adsl99801 <adsl99801@yahoo.com.tw>
2022-09-26 17:33:38 +0800

    first

---
+++ deploy/install-oddo.sh
@@ -0,0 +1,7 @@
+sudo apt-get install python3-pip
+#pip install setuptools wheel
+pip3 install -r requirements.txt
+sudo apt install postgresql -y
+sudo bash oddo.sh
+sudo systemctl start oddo-server
+
---
+++ deploy/oddo.sh
@@ -0,0 +1,3 @@
+wget -O - https://nightly.odoo.com/odoo.key | apt-key add -
+echo "deb http://nightly.odoo.com/15.0/nightly/deb/ ./" >> /etc/apt/sources.list.d/odoo.list
+apt-get update && apt-get install odoo
